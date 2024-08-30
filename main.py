19e2cd71004aec9ff116a242a995aa66f0b7bd95
harshmdev <hmohandev@gmail.com>
2024-08-30 07:10:56 +0530

    Data Ingestion Added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.movie_recommendation_project.logging import logger
+from src.movie_recommendation_project.pipeline.stage_01_data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("logging successfull")
+STAGE_NAME= "Data Ingestion Stage"
+
+try:
+    logger.info(f"started>>>>{STAGE_NAME}<<<<started")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx=====x")
+except Exception as e:
+    logger.exception(e)
+    raise e
